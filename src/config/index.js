f73c77317cf235b21c2ec5ca38db3cfbf07ddbeb
Ismael Bernal <ismaelbernal83@gmail.com>
2019-10-27 14:38:01 +0100

    Corrijo IP del fichero config para que apunte a localhost

--- src/config/index.js
+++ src/config/index.js
@@ -3,8 +3,8 @@
  * Fichero de configuración
  */
 const config = {
-    API_URL: 'http://192.168.1.33:3001/apiv1',
-    API_IMAGES: 'http://192.168.1.33:3001/',
+    API_URL: 'http://localhost:3001/apiv1',
+    API_IMAGES: 'http://localhost:3001/',
     MAX_ADVERTS_GRID: 20
 }
 
